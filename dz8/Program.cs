fb4ebb56272fbe56c318420f269fbfbd3647bde5
Nata <Beloborodova.n.s@bk.ru>
2022-07-08 00:25:57 +0300

    dz 8

---
+++ dz8/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 8:  
+int i = 0;
+int N;
+Console.WriteLine("Введите число N =: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+while (i < N)
+{
+   i += 2;
+   if (i < N)
+   Console.WriteLine (i);
+} 
+
